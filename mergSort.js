b0b32bb2e0774510aa7d5d7cbbdf4fb06056441f
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-12-16 16:55:47 -0600

    Minor edits

--- mergSort.js
+++ mergSort.js
@@ -28,24 +28,27 @@ function mergeSort(arr) {
   //mergeSort does two things: splits the arrays and then the acutal sorting
   //so we need a recursive function inside of a recursive function, like that one movie...
   //basic idea here is to recursively compare the first values of both arrays.
-  function sorter(arr1, arr2) {
+  //shift okay to use becasuse sortedLeftArr/sortedRightArr are not called again
+  function merge(arr1, arr2) {
     //base case: if both array.lenght = 0, you're done
     if (!arr1.length && !arr2.length) return [];
-    //if second array is done, return first value of first array (as an array)
+
+    //if second array is done, return first value of first array as an array ([arr1.shift()])
     //if first value of first array is less that first value of second array, return first value of first array as an array ([arr1.shift()])
     //shift first array and call recursive function, concat result
     if ((arr1.length && !arr2.length) || arr1[0] <= arr2[0]) {
-      return [arr1.shift()].concat(sorter(arr1, arr2));
+      return [arr1.shift()].concat(merge(arr1, arr2));
     }
-    //if first array is done, return first value of second array (as an array)
-    //if first value of second array is less that first value of first array, return first value of second array as an array ([arr2.shift()])
+
+    //if first array is done, return first value of second array as an array ([arr2.shift()])
+    //if first value of second array is less that first value of first array, return first value of second array as an array
     //shift second array and call recursive function, concat result
     if ((!arr1.length && arr2.length) || arr1[0] > arr2[0]) {
-      return [arr2.shift()].concat(sorter(arr1, arr2));
+      return [arr2.shift()].concat(merge(arr1, arr2));
     }
   }
 
-  const sortedArray = sorter(sortedLeftArr, sortedRightArr);
+  const sortedArray = merge(sortedLeftArr, sortedRightArr);
 
   return sortedArray;
 }
